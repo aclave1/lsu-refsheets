90cc3c9738509db0294c7bdf07d161adbe47e958
MicronX <tford9@tigers.lsu.edu>
2015-02-22 21:31:49 -0600

    second commit attempt fix drawer

--- app/src/main/java/lsu/lsureferencesheets/MainActivity.java
+++ app/src/main/java/lsu/lsureferencesheets/MainActivity.java
@@ -49,15 +49,6 @@ public class MainActivity extends ActionBarActivity
                 (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
-
-
-
-    public void goToManageClasses(View button){
-        Intent i = new Intent(this,ManageClasses.class);
-        startActivity(i);
-    }
-
-
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
@@ -70,13 +61,13 @@ public class MainActivity extends ActionBarActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.title_section1);
+                mTitle = getString(R.string.home);
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                mTitle = getString(R.string.manage_courses);
                 break;
             case 3:
-                mTitle = getString(R.string.title_section3);
+                mTitle = getString(R.string.add_cheatsheet);
                 break;
         }
     }
